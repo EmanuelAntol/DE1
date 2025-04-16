3423626da2667674a5f4dea60ad9b7c5694470a5
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-04-16 23:57:36 +0200

    Next digit signal instead of anodes, variable comments, extended comments

--- src/bcd_mux.vhd
+++ src/bcd_mux.vhd
@@ -33,58 +33,67 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity bcd_mux is
     --Generic (
-    --    N_DIGITS : integer := 3;
-    --    N_SIGNALS : integer := 2;
-    --    N_DISPLAYS : integer := 8
+    --    N_DIGITS : integer := 3;                             -- Digits used for single number
+    --    N_SIGNALS : integer := 2;                            -- Numbers displayed
     --);
-    Port ( clk : in STD_LOGIC;
-           hold : in STD_LOGIC;
-           bcd : in STD_LOGIC_VECTOR (23 downto 0);--((N_DIGITS*4)*N_SIGNALS)-1
-           bin : out STD_LOGIC_VECTOR (3 downto 0);
-           anodes : out STD_LOGIC_VECTOR (7 downto 0));--N_DISPLAYS-1
+    Port ( clk      : in STD_LOGIC;                          -- Component clock, controls the multiplexer speed (ideally 50 ms)
+           hold     : in STD_LOGIC;                          -- Hold the current displayed values
+           bcd      : in STD_LOGIC_VECTOR (23 downto 0);--((N_DIGITS*4)*N_SIGNALS)-1  -- BCD inputs for all digits
+           bin      : out STD_LOGIC_VECTOR (3 downto 0);     -- Binary output to 7 segment display component
+           anodes   : out STD_LOGIC_VECTOR (7 downto 0));--N_DISPLAYS-1  -- 7 segment display anode vector, supports 8 displays
 end bcd_mux;
 
 architecture Behavioral of bcd_mux is
-    signal sig_bcd : STD_LOGIC_VECTOR (23 downto 0); --(N_DIGITS*4-1)*N_SIGNALS
-    signal sig_anodes : STD_LOGIC_VECTOR (7 downto 0); -- choice subtype is not locally staticTerosHDL
+    signal sig_bcd : STD_LOGIC_VECTOR (23 downto 0); --(N_DIGITS*4-1)*N_SIGNALS -- BCD signal buffer
+    signal next_digit : integer range 0 to 5;                -- Next digit to display
 begin
 
     -- Main multiplexer process
+    -- Triggers on every clock input
     multiplex : process (clk) is
     begin
     
       if rising_edge(clk) then
-        -- Hold the value if hold is active
+        -- Hold the value if hold is active,
+        -- buffer new input value if hold inactive.
         if hold = '0' then
             sig_bcd <= bcd;
         end if;
     
-            -- Multiplex according to previous anode position
-            case sig_anodes is --todo unhardcode
-                when b"1011_1111" =>
-                    sig_anodes <= b"1111_1110";
+            -- Multiplex according to next digit number
+            -- 1. set the following digit
+            -- 2. separate one digit from the buffer
+            -- 3. activate corresponding display anode
+            case next_digit is --todo unhardcode
+                when 0 =>
+                    next_digit <= 1;
                     bin <= sig_bcd(3 downto 0);
-                when b"1111_1110" =>
-                    sig_anodes <= b"1111_1101";
+                    anodes <= b"1111_1110";
+                when 1 =>
+                    next_digit <= 2;
                     bin <= sig_bcd(7 downto 4);
-                when b"1111_1101" =>
-                    sig_anodes <= b"1111_1011";
+                    anodes <= b"1111_1101";
+                when 2 =>
+                    next_digit <= 3;
                     bin <= sig_bcd(11 downto 8);
-                when b"1111_1011" =>
-                    sig_anodes <= b"1110_1111";
+                    anodes <= b"1111_1011";
+                when 3 =>
+                    next_digit <= 4;
                     bin <= sig_bcd(15 downto 12);
-                when b"1110_1111" =>
-                    sig_anodes <= b"1101_1111";
+                    anodes <= b"1110_1111";
+                when 4 =>
+                    next_digit <= 5;
                     bin <= sig_bcd(19 downto 16);
-                when b"1101_1111" =>
-                    sig_anodes <= b"1011_1111";
+                    anodes <= b"1101_1111";
+                when 5 =>
+                    next_digit <= 0;
                     bin <= sig_bcd(23 downto 20);
+                    anodes <= b"1011_1111";
                 when others => --for initialization
-                    sig_anodes <= b"1011_1111";
+                    next_digit <= 0;
             end case;
 
       end if;    
     end process multiplex;
 
-    anodes <= sig_anodes;
 end Behavioral;
