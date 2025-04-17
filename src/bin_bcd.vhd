b5a79c7f6b6ed5a1164f6153c270cd5bf2ddb2b0
Karas-256754 <147066746+Karas-256754@users.noreply.github.com>
2025-04-17 13:20:40 +0200

    Add files via upload

--- src/bin_bcd.vhd
+++ src/bin_bcd.vhd
@@ -2,34 +2,28 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Entity Declaration
 entity binary_to_bcd is
     Port (
-        binary_in : in STD_LOGIC_VECTOR(8 downto 0); -- 8-bit binary input
-        bcd_out   : out STD_LOGIC_VECTOR(15 downto 0) -- 12-bit BCD output (3 decimal digits)
+        binary_in : in STD_LOGIC_VECTOR(8 downto 0); -- 8-bit binary input of distance
+        bcd_out   : out STD_LOGIC_VECTOR(11 downto 0) -- 12-bit BCD output of distance
     );
 end binary_to_bcd;
 
--- Architecture Declaration
+
 architecture Behavioral of binary_to_bcd is
 begin
     process(binary_in)
-        -- Internal variables for binary and BCD manipulation
         variable binary_value : unsigned(8 downto 0);
-        variable bcd_value    : unsigned(15 downto 0);  -- 12 bits for 3 BCD digits
+        variable bcd_value    : unsigned(11 downto 0);
         variable i            : integer;
+        
     begin
-        -- Initialize binary and BCD variables
-        binary_value := unsigned(binary_in);
-        bcd_value := (others => '0'); -- Initialize the BCD output to 0
+        binary_value := unsigned(binary_in); -- Assigning input binary input to binary value for co
+        bcd_value := (others => '0'); -- Setting the default bcd value to zero
 
-        -- Double Dabble Algorithm
+        --  shift-and-add-3 algorithm
         for i in 0 to 8 loop
             -- If the most significant digit of BCD is greater than 4, add 3
-            if bcd_value(15 downto 12) > "0100" then
-                bcd_value(15 downto 12) := bcd_value(12 downto 5) + "0011";
-            end if;
-            
             if bcd_value(11 downto 8) > "0100" then
                 bcd_value(11 downto 8) := bcd_value(11 downto 8) + "0011";
             end if;
@@ -45,7 +39,7 @@ begin
             end if;
 
             -- Shift the binary value left and add the current binary bit to the BCD
-            bcd_value := bcd_value(14 downto 0) & binary_value(8); -- Shift left
+            bcd_value := bcd_value(10 downto 0) & binary_value(8); -- Shift left
             binary_value := binary_value(7 downto 0) & '0';        -- Shift binary left
         end loop;
 
