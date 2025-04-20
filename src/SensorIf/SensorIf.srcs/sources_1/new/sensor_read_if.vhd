4339ddb66e08d826ebe06b8557eda80642b7880a
EmanuelAntol <antolemanuel03@gmail.com>
2025-04-20 16:39:21 +0200

    Added 2 new hopefully working versions of sensor read component

---
+++ src/SensorIf/SensorIf.srcs/sources_1/new/sensor_read_if.vhd
@@ -0,0 +1,71 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity sensor_read_if is
+    generic (
+        MIN_ERR_DISTANCE : integer := 5;
+        MAX_ERR_DISTANCE : integer := 400
+    );
+    port (
+        clk		    : in STD_LOGIC;                        -- 100MHZ board clock input 
+		echo		: in STD_LOGIC;                        -- Input signal from the sensors (peak width measurement)
+		oob_error	: out STD_LOGIC;                       -- Indicator for when the measured distance is too short or too long (set the bounds in MIN/MAX_ERR_DISTANCE generic varibles)
+		distance	: out STD_LOGIC_VECTOR (8 downto 0)    -- Hardware distance limit is ~2-400cm => 9 bit output is needed
+    );
+
+end sensor_read_if;
+
+architecture Behavioral of sensor_read_if is
+
+    
+    constant CM : integer := 5827;                         -- Number of clock pulses from 100MHz signal need for sound wave to travel 1cm
+    signal counter : integer range 0 to CM + 1;            -- Internal clock counter
+    signal tmp_dst_out : integer range 0 to 401;           -- Temporary distance signal
+    signal echoBuffer : STD_LOGIC := '0';
+    
+begin
+
+    sensor_read : process (clk) is         
+        begin 
+        
+        if (echo = '1' and echoBuffer = '0') then
+			echoBuffer <= '1';
+            tmp_dst_out <= 0;
+            counter <= 0; 
+        end if;
+
+        if rising_edge(clk) then
+
+            if (echo = '1' and echoBuffer = '1') then     
+                if (counter = CM) then
+                    tmp_dst_out <= tmp_dst_out + 1;
+                    counter <= 0;
+                else
+                    counter <= counter + 1;
+                end if;
+                
+            elsif (echo = '0' and echoBuffer = '1') then   
+                echoBuffer <= '0';
+                distance <= std_logic_vector(to_unsigned(tmp_dst_out, 9));
+                if (tmp_dst_out < MIN_ERR_DISTANCE or tmp_dst_out > MAX_ERR_DISTANCE) then
+                    oob_error <= '1';
+                else
+                    oob_error <= '0';
+                end if;
+                
+            else
+                distance <= std_logic_vector(to_unsigned(tmp_dst_out, 9));
+                if (tmp_dst_out < MIN_ERR_DISTANCE or tmp_dst_out > MAX_ERR_DISTANCE) then
+                    oob_error <= '1';
+                else
+                    oob_error <= '0';
+                end if;
+            end if;
+        end if;
+        
+    end process sensor_read;
+    
+
+
+end Behavioral;
---
+++ src/Sensor_readv2/Sensor_readv2.srcs/sources_1/new/Sensor_readv2.vhd
@@ -0,0 +1,103 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/20/2025 11:39:48 AM
+-- Design Name: 
+-- Module Name: Sensor_readv2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Sensor_readv2 is
+    generic (
+        MIN_ERR_DISTANCE : integer := 5;
+        MAX_ERR_DISTANCE : integer := 400
+    );
+    port (
+        clk		    : in STD_LOGIC;                        -- 100MHZ board clock input                       -- Trigger pulse (same as the sensor input)
+		echo		: in STD_LOGIC;                        -- Input signal from the sensors (peak width measurement)
+		oob_error	: out STD_LOGIC;                       -- Indicator for when the measured distance is too short or too long (set the bounds in MIN/MAX_ERR_DISTANCE generic varibles)
+		distance	: out STD_LOGIC_VECTOR (8 downto 0)    -- Hardware distance limit is ~2-400cm => 9 bit output is needed
+    );
+
+end Sensor_readv2;
+
+architecture Behavioral of Sensor_readv2 is
+    constant CM : integer := 5827;                         -- Number of clock pulses from 100MHz signal need for sound wave to travel 1cm
+    signal counter : integer range 0 to CM + 1;            -- Internal clock counter
+    signal tmp_dst_out : integer range 0 to 401;           -- Temporary distance signal
+    type state_type is (IDLE, COUNTING, WRITE);    
+    signal current_state : state_type := IDLE;             -- Defining the FSM state tracker variable and possible states
+    
+
+begin
+    read_sens : process (clk) is         
+        begin 
+        
+        case current_state is                              -- FSM process
+            when IDLE =>                                   -- "IDLE" state resets the temporary distance, counter signal and waits until the sensor echo signal is HIGH
+                counter  <= 0;
+                tmp_dst_out <= 0;
+                if (echo = '1') then
+                    current_state <= COUNTING;
+                else
+                    current_state <= WRITE;
+                end if;
+            
+                
+            when COUNTING =>
+                if (echo = '1' and counter = CM) then      -- when the amount of clock high signals ("counter") reaches a number coresponding to 1cm of lenght,
+                    tmp_dst_out <= tmp_dst_out + 1;        -- the "counter" is reset and distance counter incremented by 1 ("tmp_dst_out")
+                    counter <= 0;
+                else    
+                    counter <= counter + 1;
+                end if;
+                                                           -- "COUNTING" state counts the number of clock high signals that the "echo" is HIGH
+                if (echo = '0') then                       -- if "echo" drops to LOW, the counting stops
+                    current_state <= WRITE;
+                end if;
+                
+                
+                    
+            when WRITE =>                                  -- "WRITE" state sets the "distance" component output to the measured "tmp_dst_out" value 
+                distance <= std_logic_vector(to_unsigned(tmp_dst_out, 9));
+                                                           -- if the measured value is lower or higher than the customized limits, the "oob_error" is set to HIGH
+                if (tmp_dst_out < MIN_ERR_DISTANCE or tmp_dst_out > MAX_ERR_DISTANCE) then
+                    oob_error <= '1';
+                else
+                    oob_error <= '0';
+                end if;
+                
+                if (echo = '1') then  
+                    current_state <= IDLE;
+                end if;
+                
+                
+        end case;
+    end process read_sens;
+
+
+end Behavioral;
